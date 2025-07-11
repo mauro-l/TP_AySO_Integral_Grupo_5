32af7d2631a03d46a3911e96e59e399677348805
Mauro Laime <maurol.dev@gmail.com>
2025-07-10 22:38:54 -0300

    Upload completed resolution for alta_usuarios

--- 2_equipos/compartido_Host1/Bash_script/alta_usuarios/alta_usuarios.sh
+++ 2_equipos/compartido_Host1/Bash_script/alta_usuarios/alta_usuarios.sh
@@ -14,14 +14,20 @@ clear
 ###############################
 
 LISTA=$1
+USUARIO_ORIGEN=$2
 
 ANT_IFS=$IFS
 IFS=$'\n'
+
+PASS_HASH=$(sudo grep "^${USUARIO_ORIGEN}:" /etc/shadow | awk -F ':' '{print $2}')
+
 for LINEA in `cat $LISTA |  grep -v ^#`
 do
-	USUARIO=$(echo  $LINEA |awk -F ':' '{print $1}')
-	GRUPO=$(echo  $LINEA |awk -F ':' '{print $2}')
-	echo "sudo useradd -m -s /bin/bash -g $GRUPO $USUARIO"
+	USUARIO=$(echo  $LINEA |awk -F ',' '{print $1}')
+	GRUPO=$(echo  $LINEA |awk -F ',' '{print $2}')
+
+	sudo groupadd "$GRUPO"
+	sudo useradd -m -s /bin/bash -g $GRUPO -p "PASS_HASH" $USUARIO
 done
 IFS=$ANT_IFS
 
